b4bb963ccfded4f9e2c0ace759ef6b439812f64b
Danny Post <yayotters@gmail.com>
2016-12-12 01:29:19 -0500

    Added OTA update
    Now updating with OTA network

--- ESP8266_DHT22_WeatherStation_Thingspeak/ESP8266_DHT22_WeatherStation_Thingspeak.ino
+++ ESP8266_DHT22_WeatherStation_Thingspeak/ESP8266_DHT22_WeatherStation_Thingspeak.ino
@@ -2,6 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <DHT.h>;
 #include <Wire.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #define wifi_ssid "The Cosmos"
 #define wifi_password "jeepcherokee"
@@ -52,6 +54,28 @@ void setup_wifi() {
     Serial.print(".");
   }
 
+  ArduinoOTA.setHostname("esp8266-room");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
+  Serial.println("Ready");
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -61,6 +85,8 @@ void setup_wifi() {
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
   setup_wifi();
   Serial.print("Wifi Setup");
   dht.begin();
@@ -115,6 +141,7 @@ void publish_temp() {
     delay(60000);
 }
 void loop() {
+  ArduinoOTA.handle();
   publish_temp();
   }
 
